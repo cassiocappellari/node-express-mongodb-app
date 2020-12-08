fc29266857739aeeb48d77c0c94ed179a639f334
Cassio Cappellari <cassiocappellari@gmail.com>
2020-12-08 09:31:47 -0300

    biblioteca de criptografia adicionada ao projeto; passwords criptografados

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,8 +3,17 @@ const User = require('../models/user')
 const router = express.Router()
 
 router.post('/register', async (req, res) => {
+    const {email} = req.body
+
     try {
+
+        if (await User.findOne({email})) {
+            return res.status(400).send({error: 'user already exists'})
+        }
+
         const user = await User.create(req.body)
+
+        user.password = undefined
 
         return res.send({user})
     } catch (err) {
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -20,6 +21,13 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+})
+
+UserSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+
+    next()
 })
 
 const User = mongoose.model('User', UserSchema)
