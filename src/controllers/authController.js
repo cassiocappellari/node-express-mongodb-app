fe41849a5dc7f73405e3bc93e75fe64f2218a309
Cassio Cappellari <cassiocappellari@gmail.com>
2020-12-09 08:55:04 -0300

    arquivo de geracao de token criado; funcao de geracao de token para cada usuario cadastrado foi adicionada no authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,14 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
+const authConfig = require('../config/auth.json')
+
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400
+    })
+}
+
 router.post('/register', async (req, res) => {
     const {email} = req.body
 
@@ -17,7 +25,10 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined
 
-        return res.send({user})
+        return res.send({
+            user,
+            token: generateToken({id: user.id})
+        })
     } catch (err) {
         return res.status(400).send({error: 'registration failed'})
     }
@@ -38,9 +49,10 @@ router.post('/authenticate', async (req, res) => {
 
     user.password = undefined
 
-    const token = jwt.sign({id: user.id})
-
-    res.send({user})
+    res.send({
+        user, 
+        token: generateToken({id: user.id})
+    })
 })
 
 module.exports = app => app.use('/auth', router)
