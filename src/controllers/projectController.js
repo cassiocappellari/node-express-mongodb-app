85b2d485b6b8e050d96e2746972e868d487226d8
Cassio Cappellari <cassiocappellari@gmail.com>
2020-12-12 11:29:05 -0300

    middleware de autenticacao finalizado; middleware importado para o projectController

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const authMiddleware = require('../middlewares/auth')
+
 const router = express.Router()
+
+router.use(authMiddleware)
 
 router.get('/', (req, res) => {
     res.send({ok: true})
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,27 +1,30 @@
 const jwt = require('jsonwebtoken')
 const authConfig = require('../config/auth.json')
 
-module.exports = (req, res, next) => { // o parâmetro next é chamado somente se o usuário tiver autorização para ir ao próximo passo, que é o controller
-    const authHeader = req.headers.authorization // busca o header de autorização na requisição e armazena na variável 'authHeader'
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization
 
     if (!authHeader) {
         return res.status(401).send({error: 'no token provided'})
-    } // verifica se o token foi informado
+    }
 
-    const parts = authHeader.split(' ') // divide o token em duas partes ('parts' se transforma em um array) a partir do espaço, para verificar a palavra 'Bearer' de um lado, e o token de outro
+    const parts = authHeader.split(' ')
     
     if (!parts.lenght === 2) {
         return res.status(401).send({error: 'invalid token'})
-    } // verifica se o token está no formato correto (em duas partes), que no JWT é, por exemplo, 'Bearer + hash'
+    }
 
-    const [scheme, token] = parts // desestrutura o array 'parts' parar verificar tanto a palavra 'Bearer' quanto o próprio token
+    const [scheme, token] = parts
 
     if (!/^Bearer$/i.test(scheme)) {
         return res.status(401).send({error: 'invalid token'})
-    } // verifica, através de regex, se o 'scheme' tem a palavra Bearer escrito
+    }
 
     jwt.verify(token, authConfig.secret, (err, decoded) => {
         if (err) return res.status(401).send({error: 'invalid token'})
 
-    }) // verificação final para ver se o token da aplicação bate com o token do usuário que fez a requisição
-} // intercepa os usuaŕios para verificar se há autorização de prosseguir pela aplicação
+        req.userId = decoded.id
+
+        return next()
+    })
+}
